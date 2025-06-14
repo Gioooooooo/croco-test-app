25a1a7bcaa8614017fe744865486d55e5fee8e2b
KUPRAAA <kuprashviligiorgi3@gmail.com>
2025-06-14 15:10:27 +0400

    refactored posts ts file

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -2,10 +2,10 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule, Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { of, forkJoin } from 'rxjs';
+import { catchError, switchMap, map } from 'rxjs/operators';
 import { ApiService } from '../../services/api.service';
-import { Post } from '../../models/models';
+import { Post, User } from '../../models/models';
 
 @Component({
   selector: 'app-posts',
@@ -18,45 +18,36 @@ export class PostsComponent {
   private apiService = inject(ApiService);
   private location = inject(Location);
 
-  private params = toSignal(this.route.paramMap);
-  private userId = computed(() => this.params()?.get('id'));
+  error = signal<string | null>(null);
 
-  user = toSignal(
-    this.route.paramMap.pipe(
-      switchMap((params) => {
-        const userId = params.get('id');
-        if (userId) {
-          return this.apiService.getUser(+userId);
-        }
-        return of(null);
-      }),
-      catchError((err) => {
-        this.error.set(err.message);
-        return of(null);
-      })
-    )
+  private data$ = this.route.paramMap.pipe(
+    switchMap((params) => {
+      const userId = params.get('id');
+      if (userId) {
+        return forkJoin({
+          user: this.apiService.getUser(+userId),
+          posts: this.apiService.getPostsByUserId(+userId),
+        });
+      }
+      return this.apiService
+        .getPosts()
+        .pipe(map((posts) => ({ user: null as User | null, posts: posts })));
+    }),
+    catchError((err) => {
+      this.error.set(err.message);
+      return of({ user: null as User | null, posts: [] as Post[] });
+    })
   );
 
-  posts = toSignal(
-    this.route.paramMap.pipe(
-      switchMap((params) => {
-        const userId = params.get('id');
-        if (userId) {
-          return this.apiService.getPostsByUserId(+userId);
-        }
-        return this.apiService.getPosts();
-      }),
-      catchError((err) => {
-        this.error.set(err.message);
-        return of([]);
-      })
-    ),
-    { initialValue: [] }
-  );
+  private data = toSignal(this.data$, {
+    initialValue: { user: null as User | null, posts: [] as Post[] },
+  });
+
+  user = computed(() => this.data().user);
+  posts = computed(() => this.data().posts);
 
   selectedPost = signal<Post | null>(null);
-  isTableMode = computed(() => !this.userId());
-  error = signal<string | null>(null);
+  isTableMode = computed(() => !this.user());
 
   showPostDetails(post: Post) {
     this.selectedPost.set(post);
